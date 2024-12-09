0f567d771a00c7ffcfc0814ddbfe63d3034cc0d0
M Alamin <alamin.nava@gmail.com>
2024-12-09 14:46:02 +0600

    Click to copy app done!

---
+++ 07.Click_To_Copy_App/app.js
@@ -0,0 +1,15 @@
+const coupon = document.querySelector(".coupon")
+const btn = document.querySelector('#submitBtn')
+
+
+
+const copyText = ()=>{
+    coupon.select();
+    coupon.setSelectionRange(0,999999)
+    document.execCommand("copy")
+    btn.innerHTML = "Copied!"
+    setTimeout(()=>{
+        btn.textContent = "Check To Copy"
+    },2000)
+}
+btn.addEventListener("click", copyText)
